d741f1678021f1cc4eca3e77a575b4fcb37d2405
Juliano Stulp <juliano_stulp@hotmail.com>
2023-06-27 21:19:09 -0300

    Criado classe de Tabuleiro e UI

---
+++ src/Xadrez/PecaXadrez.java
@@ -0,0 +1,20 @@
+package Xadrez;
+
+import Tabuleiro.Peca;
+import Tabuleiro.Tabuleiro;
+
+public class PecaXadrez extends Peca{
+	
+	private Cor cor;
+
+	public PecaXadrez(Tabuleiro tabuleiro, Cor cor) {
+		super(tabuleiro);
+		this.cor = cor;
+	}
+
+	public Cor getCor() {
+		return cor;
+	}
+
+	
+}
--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,13 +1,14 @@
 package aplicacao;
 
-import Tabuleiro.Posicao;
-import Tabuleiro.Tabuleiro;
+import Xadrez.PartidaXadrez;
 
 public class Programa {
 
 	public static void main(String[] args) {
 
-		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		PartidaXadrez partidaXadrez = new PartidaXadrez();
+		
+		UI.imprimePartida(partidaXadrez.getPecas());
 
 	}
 
